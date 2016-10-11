e6b36f8e3a31e0ccaa5609e8b47d2abe061fecf6
Stacy Ozorio <stacyozorio@Stacys-MBP.T-mobile.com>
2016-10-10 22:53:54 -0700

    new views.py

--- mysite/mysite/views/views.py
+++ mysite/mysite/views/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import MySQLdb 
 
-db = MySQLdb.connect("localhost", "root", "", "test")       
+db = MySQLdb.connect("localhost", "root", "", "test")  #database     
 
 cur = db.cursor()
 
-db.query("SELECT name FROM main WHERE id=4;")
-r = db.store_result();
+db.query("SELECT * FROM main;")	#query stmt from database
+r = db.store_result();	#view data from query
 
 db.query("""INSERT INTO main (id, name, score) VALUES (null, 'Stacy', 100);""")
-db.commit()
+db.commit()	#actually does query, writes in database
 
 # Create your views here.
 def index(request):
-	return HttpResponse(r.fetch_row())
+	return HttpResponse(r.fetch_row(0))	#get this working on website
